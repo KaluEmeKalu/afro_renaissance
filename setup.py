f4b54c0cf2ce2b1b8df74019711fd889e361f794
Kalu Kalu <kalu@Kalus-MBP.attlocal.net>
2025-02-04 19:02:11 -0800

    Skip package installation and run Django commands directly

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="afro_renaissance",
     version="1.0.0",
-    packages=["afro_renaissance", "blog", "manifesto", "social"],
+    packages=["afro_renaissance"],
     include_package_data=True,
 )
