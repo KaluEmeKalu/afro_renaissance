ea6cc64558770f0a7a88878c19b9ea9d2a48a372
Kalu Kalu <kalu@Kalus-MBP.attlocal.net>
2025-02-04 19:00:05 -0800

    Simplify package configuration and installation

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@ from setuptools import setup
 setup(
     name="afro_renaissance",
     version="1.0.0",
-    packages=['afro_renaissance'],
+    packages=["afro_renaissance", "blog", "manifesto", "social"],
     include_package_data=True,
-    install_requires=[
-        line.strip()
-        for line in open("requirements.txt")
-        if line.strip() and not line.startswith("#")
-    ],
-    python_requires=">=3.11",
 )
